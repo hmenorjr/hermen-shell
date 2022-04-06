7cfdbad251d1c366c7b8ad11b59f8b4141a2e963
Herman Menor <herman.menor@gmail.com>
2022-04-06 09:27:47 +0300

    Create openSites.sh
    A bash-script that opens a Chrome browser in incognito mode with 3 tabs containing the sites you want to open.

---
+++ openSites.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+open -a "Google Chrome" -n --args --incognito "https://www.google.com"
+open -a "Google Chrome" -n --args --incognito "https://www.facebook.com"
+open -a "Google Chrome" -n --args --incognito "https://www.twitter.com"
